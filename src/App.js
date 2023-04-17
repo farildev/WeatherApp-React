b98a8b38fa377c8e9900420c2aaf00972702a01c
farildev <memmedovfaril@gmail.com>
2023-04-17 20:24:24 +0400

    UPDATE

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from "react";
-
+import "./assets/styles/app.css"
 function App() {
   return (
     <div className="App">
-      
+      <div className="form__control container">
+        <input type="text" placeholder="Enter the city name"/>
+      </div>
+      <div className="center__content container">
+      <div className="content">
+        <div className="city__name">
+          <h1>Denver</h1>
+        </div>
+        <div className="city__degree">
+          <span>65 C</span>
+        </div>
+      </div>
+      <div className="bottom__content">
+        <div className="">
+
+        </div>
+      </div>
+      </div>
     </div>
   );
 }
